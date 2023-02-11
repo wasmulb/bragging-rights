25a817a69adb2bc1853cc8d39f3ff499b801dadd
Levi Wasmundt <leviwasmundt@gmail.com>
2023-02-11 15:54:37 -0800

    added front end functionality to create partnerships

--- public/js/competitors.js
+++ public/js/competitors.js
@@ -1,21 +1,64 @@
-var compSection = document.getElementsByClassName('comp-section')
+//For displaying the logged in users competitors
+// var compSection = document.getElementsByClassName('comp-section')
 
-var userID = 1
+// var userID = 1
 
-function viewCompetitors(){
-    fetch('/api/partners')
-    .then(function(response) {
-      return response.json()
+// function viewCompetitors(){
+//     fetch('/api/partners')
+//     .then(function(response) {
+//       return response.json()
+//     })
+//     .then(function (data) {
+//         console.log(data)
+
+//         for(var i= 0; i<data.length; i++){
+//             var compTag = document.createElement('a')
+//             compTag.classList.add("comp-tag")
+//             compTag.innerText = data.partners;
+//             //select from data
+
+//             compSection.appendChild(compTag)
+//         };
+// })}
+
+
+
+const addCompetitor = async (event) => {
+  event.preventDefault();
+
+  let firstUser = document.querySelector('#first-username').value.trim();
+  let secondUser = document.querySelector('#second-username').value.trim();
+
+  let firstUserID = await fetch(`/api/users/${firstUser}`)
+    .then(function (res) {
+      return res.json()
     })
-    .then(function (data) {
-        console.log(data)
+    .then(function (dataOne) {
+      return dataOne.id
+    })
 
-        for(var i= 0; i<data.length; i++){
-            var compTag = document.createElement('a')
-            compTag.classList.add("comp-tag")
-            compTag.innerText = data.partners;
-            //select from data
+  let secondUserID = await fetch(`/api/users/${secondUser}`)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (dataTwo) {
+      return dataTwo.id
+    })
 
-            compSection.appendChild(compTag)
-        };
-})}
+    console.log('test!',firstUserID, secondUserID)
+
+  const response = await fetch('/api/partners', {
+    method: 'POST',
+    body: JSON.stringify({ firstUserID, secondUserID }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  if (response.ok) {
+    alert('Partnership Created!')
+  } else {
+    alert('Failed to create partnership')
+  }
+}
+
+document.querySelector(".new-comp-form").addEventListener('submit', addCompetitor)
