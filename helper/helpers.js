48751949c376543c6e7376a0de446cba3cec4ead
Levi Wasmundt <leviwasmundt@gmail.com>
2023-02-14 14:25:38 -0800

    added helper folder and removed many console logs

---
+++ helper/helpers.js
@@ -0,0 +1,67 @@
+async function findUserByUserID(userid) {
+    console.log(userid)
+    const user = await User.findOne({
+      where: {
+        id: userid
+      },
+    });
+    console.log(user)
+    return user;
+  }
+
+  async function findUsersByPartnerID(partnerID, userID) {
+    console.log(partnerID)
+    let partnerData = await UserPartners.findAll({
+      where: {
+        partners_id: partnerID,
+      },
+      // include: [{ model: UserPartners}],
+    });
+    console.log(partnerData)
+    partnerData = partnerData.filter(partner => partner.dataValues.user_id != userID)
+    // partnerData = partnerData.map(async (user) => await findUserByUserID(user.dataValues.user_id))
+    let result = []
+    for (let i = 0; i<partnerData.length; i++){
+      result.push(await findUserByUserID(partnerData[i].dataValues.user_id))
+    }
+    return result;
+  }
+
+    async function findUserByUserID(userid) {
+    console.log(userid)
+    const user = await User.findOne({
+      where: {
+        id: userid
+      },
+    });
+    console.log(user)
+    return user;
+  }
+
+    async function findPartnersByUserID(userID) {
+    const userPartners = await UserPartners.findAll({
+      where: {
+        user_id: userID
+      },
+      // include: [{ model: UserPartners}],
+    });
+    return userPartners;
+  }
+
+  async function findUserByUsername(username) {
+    const user = await User.findOne({
+      where: {
+        username: username
+      },
+      include: [{ model: UserPartners}],
+    });
+    return user;
+  }
+
+  module.exports = {
+    findUserByUserID,
+    findUsersByPartnerID,
+    findUserByUserID,
+    findPartnersByUserID,
+    findUserByUsername,
+  };
